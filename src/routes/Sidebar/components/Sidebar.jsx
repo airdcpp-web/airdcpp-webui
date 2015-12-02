07e495e85e4f7456690559c6fef4613d0ffeaad1
maksis <maksis@adrenaline-network.com>
2015-12-02 13:23:37 +0200

    Render the sidebar content only after the sidebar has finished animating
    Avoids issues if there are router transitions while the sidebar is animating (such as placing the sidebar in the middle of the window).
    
    TODO: check the need for router transitions before starting animating?

--- src/routes/Sidebar/components/Sidebar.jsx
+++ src/routes/Sidebar/components/Sidebar.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 
 import OverlayDecorator from 'decorators/OverlayDecorator';
+import Loader from 'components/semantic/Loader';
 
 import '../style.css';
 
@@ -10,15 +11,25 @@ const Sidebar = React.createClass({
 	componentDidMount() {
 		this.props.showOverlay({
 			context: '#main-layout',
-			delaySetup: true
+			onShow: this.onShow,
 		});
+	},
+
+	getInitialState() {
+		return {
+			visible: false,
+		};
+	},
+
+	onShow() {
+		this.setState({ visible: true });
 	},
 
 	render() {
 		return (
 			<div id="sidebar" className="ui right vertical overlay sidebar">
 				<div id="sidebar-container">
-					{ this.props.children }
+					{ this.state.visible ? this.props.children : <Loader text=""/> }
 				</div>
 			</div>
 		);
