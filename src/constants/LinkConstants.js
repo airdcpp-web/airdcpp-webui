32ec0bfc42886a5aadd8e059b77d1a946edcaaae
maksis <maksis@adrenaline-network.com>
2016-05-08 10:23:22 +0300

    New home page URL

--- src/constants/LinkConstants.js
+++ src/constants/LinkConstants.js
@@ -1,5 +1,5 @@
 export default {
-	HOME_PAGE_URL:	'https://github.com/airdcpp-web/airdcpp-webclient',
+	HOME_PAGE_URL:	'https://airdcpp-web.github.io',
 
 	SPEEDTEST_URL:	'http://www.speedtest.net',
 	LOG_HELP_URL: 	'http://dcplusplus.sourceforge.net/webhelp/settings_logs.html'
