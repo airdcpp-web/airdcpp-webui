cc17e52d0feb37078d94986d6e3d5e1efec55d6b
maksis <maksis@adrenaline-network.com>
2016-08-02 16:47:42 +0300

    Add a help message for download target variables

--- src/routes/Settings/routes/Downloads/components/LocationsPage.jsx
+++ src/routes/Settings/routes/Downloads/components/LocationsPage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import ExternalLink from 'components/ExternalLink';
+import LinkConstants from 'constants/LinkConstants';
+import Message from 'components/semantic/Message';
+
 import SettingForm from 'routes/Settings/components/SettingForm';
 import SettingPageMixin from 'routes/Settings/mixins/SettingPageMixin';
 
@@ -18,9 +22,40 @@ const LocationsPage = React.createClass({
 					ref="form"
 					formItems={Entry}
 				/>
+
+				<Message 
+					description={
+						<span>
+							<div className="ui small header">Path variables</div>
+
+							<ul>
+								<li>
+									<strong>%[username]</strong> - Replace with the name of the web user who queued the item (or with the system username if the download is initiated from the Windows GUI)
+								</li>
+							</ul>
+
+							<p>
+								<ExternalLink url={ LinkConstants.LOG_HELP_URL }>Available time formating variables</ExternalLink>
+							</p>
+
+							<p>
+								<strong>
+									Example:
+								</strong>
+								<br/>
+								<i>/home/%[username]/Downloads/%Y-%m-%d/</i> (will be replaced with <i>/home/mywebuser/Downloads/2016-08-02/</i>)
+							</p>
+						</span>
+					}
+				/>
 			</div>
 		);
 	}
 });
+
+/*							<div className="ui large header">
+								<i className="blue info icon"/>
+								Path variables
+							</div>*/
 
 export default LocationsPage;
