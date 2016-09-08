9e5b881a8746301fc2448574315ca449e971c904
maksis <maksis@adrenaline-network.com>
2016-09-08 13:15:30 +0300

    Remove an unused file

--- src/actions/SocketActions.js
+++
@@ -1,7 +0,0 @@
-import Reflux from 'reflux';
-
-// Triggered by API socket
-export default Reflux.createActions([
-	{ 'state': { children: [ 'disconnected', 'connecting', 'connected' ] } },
-	'message'
-]);
