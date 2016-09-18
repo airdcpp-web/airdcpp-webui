c5d0616996cb936e152005b42c727ac9708ccdf3
maksis <maksis@adrenaline-network.com>
2016-09-18 22:14:08 +0300

    Revert "Migrate StatisticsIcons to use DataProviderDecorator" (it just complicated things)
    This reverts commit 84209987171bcc89728a0b34f2173bc1af34cbd1.

--- src/components/main/navigation/icon/StatisticsIcons.jsx
+++ src/components/main/navigation/icon/StatisticsIcons.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import classNames from 'classnames';
 
-import DataProviderDecorator from 'decorators/DataProviderDecorator';
+import SocketService from 'services/SocketService';
 import ValueFormat from 'utils/ValueFormat';
+import SocketSubscriptionMixin from 'mixins/SocketSubscriptionMixin';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 import HashConstants from 'constants/HashConstants';
@@ -11,7 +11,7 @@ import IconConstants from 'constants/IconConstants';
 
 
 const StatisticsIcon = ({ icon, bytes, formatter }) => {
-	if (!bytes) {
+	if (bytes === 0) {
 		return null;
 	}
 
@@ -19,43 +19,70 @@ const StatisticsIcon = ({ icon, bytes, formatter }) => {
 		<div className="item">
 			<i className={ icon + ' icon'}/>
 			<div className="content">
-				<div className="header">
-					{ formatter(bytes) }
-				</div>
+				<div className="header">{ formatter(bytes) }</div>
 			</div>
 		</div>
 	);
 };
 
 const StatisticsBar = React.createClass({
-	mixins: [ PureRenderMixin ],
+	mixins: [ PureRenderMixin, SocketSubscriptionMixin() ],
+	getDefaultProps() {
+		return {
+			className: '',
+		};
+	},
+
+	onSocketConnected(addSocketListener) {
+		addSocketListener(TransferConstants.MODULE_URL, TransferConstants.STATISTICS, this.onStatsReceived);
+		addSocketListener(HashConstants.MODULE_URL, HashConstants.STATISTICS, this.onStatsReceived);
+	},
+
+	fetchStats() {
+		SocketService.get(TransferConstants.STATISTICS_URL)
+			.then(this.onStatsReceived)
+			.catch(error => console.error('Failed to fetch transfer statistics', error.message));
+	},
+
+	componentDidMount() {
+		this.fetchStats();
+	},
+
+	onStatsReceived(data) {
+		const newState = Object.assign({}, this.state, data);
+		this.setState(newState);
+	},
+
+	getInitialState() {
+		return {
+			speed_down: 0,
+			speed_up: 0,
+			hash_speed: 0,
+			queued_bytes: 0,
+		};
+	},
+
 	render: function () {
-		const { stats, className } = this.props;
 		return (
-			<div 
-				className={ classNames(
-					'ui centered inverted mini list statistics-icons', 
-					className
-				) }
-			>
+			<div className={ 'ui centered inverted mini list statistics-icons ' + this.props.className }>
 				<StatisticsIcon 
 					icon={ IconConstants.DOWNLOAD }
-					bytes={ stats.speed_down }
+					bytes={ this.state.speed_down }
 					formatter={ ValueFormat.formatSpeed }
 				/>
 				<StatisticsIcon 
 					icon={ IconConstants.UPLOAD }
-					bytes={ stats.speed_up }
+					bytes={ this.state.speed_up }
 					formatter={ ValueFormat.formatSpeed }
 				/>
 				<StatisticsIcon 
 					icon={ IconConstants.HASH }
-					bytes={ stats.hash_speed }
+					bytes={ this.state.hash_speed }
 					formatter={ ValueFormat.formatSpeed }
 				/>
 				<StatisticsIcon 
 					icon={ IconConstants.QUEUE }
-					bytes={ stats.queued_bytes }
+					bytes={ this.state.queued_bytes }
 					formatter={ ValueFormat.formatSize }
 				/>
 			</div>
@@ -63,17 +90,4 @@ const StatisticsBar = React.createClass({
 	}
 });
 
-export default DataProviderDecorator(StatisticsBar, {
-	urls: {
-		stats: TransferConstants.STATISTICS_URL,
-	},
-	onSocketConnected: (addSocketListener, { mergeData }) => {
-		const mergeStats = stats => mergeData({
-			stats,
-		});
-
-		addSocketListener(TransferConstants.MODULE_URL, TransferConstants.STATISTICS, mergeStats);
-		addSocketListener(HashConstants.MODULE_URL, HashConstants.STATISTICS, mergeStats);
-	},
-	loaderText: null,
-});
+export default StatisticsBar;
