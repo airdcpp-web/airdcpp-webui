66863e4ad090f0360bacd251c5ca364c0ef3d7d2
maksis <maksis@adrenaline-network.com>
2016-09-19 22:07:12 +0300

    Make the sidebar resizable
    Fixes airdcpp-web/airdcpp-webclient#30
    References airdcpp-web/airdcpp-webclient#117

--- src/routes/Sidebar/components/Sidebar.jsx
+++ src/routes/Sidebar/components/Sidebar.jsx
@@ -1,13 +1,14 @@
-'use strict';
-
 import React from 'react';
+import ReactDOM from 'react-dom';
 import invariant from 'invariant';
 
-import OverlayDecorator from 'decorators/OverlayDecorator';
-import Loader from 'components/semantic/Loader';
 import BrowserUtils from 'utils/BrowserUtils';
+import Loader from 'components/semantic/Loader';
+import OverlayDecorator from 'decorators/OverlayDecorator';
+import Resizable from 'react-resizable-box';
 
 import '../style.css';
+
 
 const Sidebar = React.createClass({
 	propTypes: {
@@ -29,9 +30,10 @@ const Sidebar = React.createClass({
 	},
 
 	getInitialState() {
-		// Don't render the content while sidebar is animating
-		// Avoids issues if there are router transitions while the sidebar is animating (such as placing the content in the middle of the window)
+		this.initialWidth = BrowserUtils.loadLocalProperty('sidebar_width', 1000);
 		return {
+			// Don't render the content while sidebar is animating
+			// Avoids issues if there are router transitions while the sidebar is animating (such as placing the content in the middle of the window)
 			visible: false,
 		};
 	},
@@ -42,13 +44,34 @@ const Sidebar = React.createClass({
 		}
 	},
 
+	onResizeStop(direction, styleSize, { width }) {
+		BrowserUtils.saveLocalProperty('sidebar_width', width);
+	},
+
 	render() {
+		// Ref hack because of https://github.com/bokuweb/react-resizable-box/issues/45
 		return (
-			<div id="sidebar" className="ui right vertical sidebar">
+			<Resizable
+				ref={ c => {
+					if (c) {
+						ReactDOM.findDOMNode(c).setAttribute('id', 'sidebar');
+					}
+				} }
+				width={ Math.min(this.initialWidth, window.innerWidth) }
+				height={ window.innerHeight }
+				minWidth={ 500 }
+				maxWidth={ window.innerWidth }
+				customClass="ui right vertical sidebar" 
+
+				isResizable={
+					{ top:false, right:false, bottom:false, left:true, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }
+				}
+				onResizeStop={ this.onResizeStop }
+			>
 				<div id="sidebar-container">
 					{ this.state.visible ? this.props.children : <Loader text=""/> }
 				</div>
-			</div>
+			</Resizable>
 		);
 	},
 });
