ee06a68d787f96b4ddb8e684e778beebd1f4d53b
maksis <maksis@adrenaline-network.com>
2016-10-08 14:10:30 +0300

    http -> https

--- src/constants/LinkConstants.js
+++ src/constants/LinkConstants.js
@@ -2,5 +2,5 @@ export default {
 	HOME_PAGE_URL:	'https://airdcpp-web.github.io',
 
 	SPEEDTEST_URL:	'http://www.speedtest.net',
-	VARIABLE_HELP_URL: 	'http://www.airdcpp.net/component/k2/25-setting-parameters'
+	VARIABLE_HELP_URL: 	'https://www.airdcpp.net/component/k2/25-setting-parameters'
 };
