9956785ede3ed995d9b68235847be8a5f0f3a068
maksis <maksis@adrenaline-network.com>
2016-10-10 12:16:04 +0300

    Add global error handler for async chunk loading errors

--- src/utils/webpack.js
+++ src/utils/webpack.js
@@ -1,7 +1,18 @@
+import NotificationActions from 'actions/NotificationActions';
+
 // The webpack path must be set before loading any styles
 // Since common styles are imported in the main entry, this must be done in a separate import
 // https://github.com/webpack/css-loader/issues/155
 __webpack_public_path__ = getBasePath();
+
+// Override the default onError function
+__webpack_require__.oe = (err) => { 
+	NotificationActions.error({ 
+		title: err,
+		message: 'Try reloading the page if you have updated the UI recently',
+	});
+};
+
 
 // We must export something or this module gets stripped
 export default __webpack_public_path__;
