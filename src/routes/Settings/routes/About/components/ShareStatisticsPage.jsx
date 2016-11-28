840d544d46d7a04ce5bc59963a2400d2aba82869
maksis <maksis@adrenaline-network.com>
2016-11-28 21:09:32 +0200

    Share stats: show search matching statistics

--- src/routes/Settings/routes/About/components/ShareStatisticsPage.jsx
+++ src/routes/Settings/routes/About/components/ShareStatisticsPage.jsx
@@ -9,7 +9,7 @@ import ShareConstants from 'constants/ShareConstants';
 
 import StatisticsDecorator from 'decorators/StatisticsDecorator';
 
-import { Row } from './Grid';
+import { Row, Header } from './Grid';
 
 const ShareStatisticsPage = React.createClass({
 	render() {
@@ -22,6 +22,20 @@ const ShareStatisticsPage = React.createClass({
 				<Row title="Total directories" text={stats.total_directory_count}/>
 				<Row title="Average file age" text={averageFileAge}/>
 				<Row title="Average files per directory" text={stats.files_per_directory.toFixed(1)}/>
+
+				<Header title="Incoming searches"/>
+				<Row title="Total searches" text={ stats.total_searches + ' (' + stats.total_searches_per_second.toFixed(1) + ' per second)' }/>
+				<Row title="TTH searches" text={stats.tth_search_percentage.toFixed(2) + '%' }/>
+				<Row title="Text searches" text={ stats.total_recursive_searches + ' (' + stats.unfiltered_recursive_searches_per_second.toFixed(2) + ' matched per second)' }/>
+				<Row 
+					title="Filtered text searches" 
+					text={ stats.filtered_search_percentage.toFixed(2) + '% (' + stats.unfiltered_recursive_match_percentage.toFixed(2) + '% of the matched ones returned results)' }
+				/>
+				<Row 
+					title="Average text search tokens (non-filtered)" 
+					text={ stats.average_search_token_count.toFixed(1) + ' (' + stats.average_search_token_length.toFixed(1) + ' bytes per token)' }
+				/>
+				<Row title="Average matching time per text search" text={stats.average_match_ms + ' ms' }/>
 			</div>
 		);
 	},
