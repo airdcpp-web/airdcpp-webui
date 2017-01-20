edd38c92457be878a400f5713b9811a992bc66e1
maksis <maksis@adrenaline-network.com>
2017-01-20 19:39:37 +0200

    Show API versions on About page

--- src/routes/Settings/routes/About/components/AboutPage.jsx
+++ src/routes/Settings/routes/About/components/AboutPage.jsx
@@ -24,6 +24,8 @@ const AboutPage = React.createClass({
 				<Row title="Application version" text={ systemInfo.client_version }/>
 				<Row title="Web UI version" text={ UI_VERSION }/>
 				<Row title="Web UI build date" text={ buildDate }/>
+				<Row title="API version" text={ systemInfo.api_version }/>
+				<Row title="API feature level" text={ systemInfo.api_feature_level }/>
 				<Row title="Started" text={ ValueFormat.formatRelativeTime(systemInfo.client_started) }/>
 				<Row title="Active sessions" text={ stats.active_sessions }/>
 				<Row title="Server threads" text={ stats.server_threads }/>
