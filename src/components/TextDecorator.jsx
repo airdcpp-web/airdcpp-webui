3e0e09a2e7db2b5ee5963978a4bf0050f865b448
maksis <maksis@adrenaline-network.com>
2017-02-09 12:03:04 +0200

    Detect magnet links and perform search when clicking on them (closes airdcpp-web/airdcpp-webclient#187)
    airdcpp-web/airdcpp-webclient#3 is still causing problems though

--- src/components/TextDecorator.jsx
+++ src/components/TextDecorator.jsx
@@ -2,21 +2,59 @@
 
 import React from 'react';
 
-import Linkify from 'react-linkify';
+import { default as ReactLinkify, linkify } from 'react-linkify';
 import ReactEmoji from 'react-emoji';
+import History from 'utils/History';
+
+
+linkify.add('magnet:', {
+	validate: (text, pos, self) => {
+		const tail = text.slice(pos);
+		if (!self.re.magnet) {
+			self.re.magnet = new RegExp('^(\\?xt=.{64,})');
+		}
+
+		if (self.re.magnet.test(tail)) {
+			return tail.match(self.re.magnet)[0].length;
+		}
+
+		return 0;
+	}
+});
+
+
+const onClickLink = (evt, routerLocation) => {
+	const uri = evt.target.href;
+	if (uri.indexOf('magnet:?xt=urn:tree:tiger:') === 0) {
+		evt.preventDefault();
+
+		const tth = uri.slice(26, 26 + 39);
+		History.pushUnique({
+			pathname: '/search',
+			state: {
+				searchString: tth,
+			}
+		}, routerLocation);
+	}
+};
 
 // Parses links from plain text and optionally emoticons as well
-const TextDecorator = ({ emojify = false, text }) => (
-	<Linkify 
+const TextDecorator = ({ emojify = false, text }, { routerLocation }) => (
+	<ReactLinkify 
 		properties={{ 
 			target: '_blank',
 			rel: 'noreferrer',
+			onClick: evt => onClickLink(evt, routerLocation),
 		}}
 	>
 		{ !emojify ? text : ReactEmoji.emojify(text, {
 			emojiType: 'twemoji'
 		})}
-	</Linkify>
+	</ReactLinkify>
 );
+
+TextDecorator.contextTypes = {
+	routerLocation: React.PropTypes.object.isRequired,
+};
 
 export default TextDecorator;
