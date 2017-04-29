d67cdc8c22d9ed319ac130e27047f84f4406a8ca
maksis <maksis@adrenaline-network.com>
2017-04-29 11:52:41 +0300

    Add help message for connectivity troubleshooting

--- src/constants/LinkConstants.js
+++ src/constants/LinkConstants.js
@@ -2,6 +2,9 @@ export default {
 	HOME_PAGE_URL:	'https://airdcpp-web.github.io',
 
 	SPEEDTEST_URL:	'http://www.speedtest.net',
+	PORTCHECK_URL:	'http://www.yougetsignal.com/tools/open-ports/',
+	CONNECTIVITY_HELP_URL: 'http://dcplusplus.sourceforge.net/webhelp/faq_connection.html',
+
 	VARIABLE_HELP_URL: 	'https://www.airdcpp.net/component/k2/25-setting-parameters',
 
 	DEFAULT_ENGINE_URL: 'https://www.nodejs.org',
--- src/routes/Settings/routes/Connectivity/components/DetectionPage.jsx
+++ src/routes/Settings/routes/Connectivity/components/DetectionPage.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 
 import DetectPanel from './DetectPanel';
 
+import ExternalLink from 'components/ExternalLink';
+import LinkConstants from 'constants/LinkConstants';
+import Message from 'components/semantic/Message';
+
 
 const DetectionPage = React.createClass({
 	render() {
 		return (
 			<div>
 				<DetectPanel/>
+
+				<Message 
+					description={
+						<span>
+							In case of file transfer connectivity issues, please confirm that your TCP ports are really open by using <ExternalLink url={ LinkConstants.PORTCHECK_URL }>an online port checker</ExternalLink>.
+							Note that validity of the UDP port (search) can't be checked online.
+
+							For more information about different connectivity modes, please visit <ExternalLink url={ LinkConstants.CONNECTIVITY_HELP_URL }>DC++'s connectivity FAQ</ExternalLink>.
+						</span>
+					}
+					icon="blue info"
+				/>
 			</div>
 		);
 	}
