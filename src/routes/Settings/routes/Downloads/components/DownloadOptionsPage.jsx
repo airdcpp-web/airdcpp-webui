aa446f2cbc13ee2c0a6b62e594276784d87ec47b
maksis <maksis@adrenaline-network.com>
2017-12-27 14:19:34 +0200

    Remove 'new_segment_min_speed' setting

--- src/routes/Settings/routes/Downloads/components/DownloadOptionsPage.jsx
+++ src/routes/Settings/routes/Downloads/components/DownloadOptionsPage.jsx
@@ -4,7 +4,6 @@ import RemoteSettingForm from 'routes/Settings/components/RemoteSettingForm';
 const Entry = [
   'segmented_downloads',
   'min_segment_size',
-  'new_segment_min_speed',
   'allow_slow_overlap',
   'finished_remove_exit',
   'use_partial_sharing',
