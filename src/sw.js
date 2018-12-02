ae0d528c4ded583dbab467c0397a28aa5456e2c6
maksis <maksis@adrenaline-network.com>
2018-12-02 14:31:26 +0200

    Service worker: try a fetch a fresh index.html before returning a cached one

--- src/sw.js
+++ src/sw.js
@@ -8,12 +8,13 @@ const assets = global.serviceWorkerOption.assets
   .map(mapAsset);
 
 const DEBUG = true;
+const CACHE_NAME = 'resource-store';
 
 self.addEventListener('install', e => {
   console.log('[SW] Worker installed');
 
   e.waitUntil(
-    caches.open('resource-store')
+    caches.open(CACHE_NAME)
       .then(cache => cache.addAll([ ...assets ]))
       .then(() => {
         if (DEBUG) {
@@ -66,33 +67,55 @@ self.addEventListener('activate', event => {
       )
     })
   )
-})
+});
+
+function respondCache(e, cacheUrl) {
+  return caches.match(new URL(cacheUrl)).then(function(response) {
+    if (!!response) {
+      if (DEBUG) {
+        console.log('[SW] Return cached', e.request.url);
+      }
+
+      return response;
+    }
+
+    if (DEBUG) {
+      console.log('[SW] Fetch', e.request.url);
+    }
+
+    return fetch(e.request);
+  });
+}
 
  
 self.addEventListener('fetch', e => {
-  let cacheUrl = e.request.url;
-  if (e.request.url.startsWith(basePath) &&
-    !e.request.url.replace(basePath, '').match(/^(view|assets|js|images)\//)
+  if (e.request.mode === 'navigate' || (
+      e.request.url.startsWith(basePath) &&
+      e.request.url.indexOf('.', basePath.length) === -1 // no file extension?
+    )
   ) {
-    // Routing URL
-    cacheUrl = basePath + 'index.html';
-  } 
+    console.log('[SW] Navigate action, fetch index', e.request.url);
+    const cacheUrl = basePath + 'index.html';
 
-  e.respondWith(
-    caches.match(new URL(cacheUrl)).then(function(response) {
-      if (!!response) {
-        if (DEBUG) {
-          console.log('[SW] Return cached', e.request.url);
-        }
+    // Try to fetch index from the server, fall back to using a cached version if it fails
+    return e.respondWith(
+      new Promise((resolve, reject) => {
+        fetch(cacheUrl).then(res => {
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(new Request(cacheUrl), res.clone()).then(() => {
+                console.log('[SW] Index file cached', e.request.url);
+                resolve(res);
+              });
+            });
+          })
+          .catch(err => {
+            console.log('[SW] Navigate action, failed to fetch index', e.request.url, err);
+            resolve(respondCache(e, cacheUrl));
+          });
+      })
+    );
+  }
 
-        return response;
-      }
-
-      if (DEBUG) {
-        console.log('[SW] Fetch', e.request.url);
-      }
-
-      return fetch(e.request);
-    })
-  );
+  return e.respondWith(respondCache(e, e.request.url));
 });
