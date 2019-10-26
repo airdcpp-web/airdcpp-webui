b1f06b6e0a67aff8ea18c673f1ab80b5214a4ea5
maksis <maksis@adrenaline-network.com>
2019-10-27 00:03:02 +0300

    New encryption settings: tls_allow_untrusted_clients and tls_allow_untrusted_hubs

--- src/routes/Settings/routes/Connectivity/components/EncryptionPage.tsx
+++ src/routes/Settings/routes/Connectivity/components/EncryptionPage.tsx
@@ -6,6 +6,8 @@ import { SettingSectionChildProps } from 'routes/Settings/components/SettingSect
 const Entry = [
   'always_ccpm',
   'tls_mode',
+  'tls_allow_untrusted_clients',
+  'tls_allow_untrusted_hubs',
 ];
 
 const Encryption: React.FC<SettingSectionChildProps> = props => (
