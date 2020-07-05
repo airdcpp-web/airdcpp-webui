84dbfaca55168fd2caedeae33dfcd5d23e171a6f
Jonas Marklén <jonas.marklen@sitedirect.se>
2020-07-05 20:50:01 +0200

    updates for localstorage and some styling

--- src/widgets/Notepad/components/Notepad.tsx
+++ src/widgets/Notepad/components/Notepad.tsx
@@ -3,50 +3,56 @@ import React from 'react';
 import '../style.css';
 import * as UI from 'types/ui';
 import { loadLocalProperty, saveLocalProperty } from 'utils/BrowserUtils';
-import { widgetIdToSettingKey } from 'utils/WidgetUtils';
+import { widgetIdToLocalStateKey } from 'utils/WidgetUtils';
 
 export type NotepadProps = UI.WidgetProps;
-export type NotepadState = UI.WidgetSettings;
+export type NotepadState = UI.WidgetSettings; 
 
 class Notepad extends React.PureComponent<NotepadProps, NotepadState> {
+  key: string;
 
   constructor(props: any) {
     super(props);
 
     const { componentId } = this.props;
+    this.key = widgetIdToLocalStateKey(componentId);
 
-    const defaultData = {
-      widget : {
-        value : ''
-      }
-    };
-
-    let state: any = loadLocalProperty(widgetIdToSettingKey(componentId), defaultData);
+    let state: any = { widget: this.getStoredState() };
     this.state = state;
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event: any) {
-    const { componentId } = this.props;
+    const data = {
+      content: event.target.value
+    };
+    saveLocalProperty(this.key, data);
+    this.setState({widget: data});
+  }
 
-    this.setState({widget : { value: event.target.value } } );
-    saveLocalProperty(widgetIdToSettingKey(componentId), this.state);
+  getStoredState() {
+    const defaultData = {
+      content : ''
+    };
+
+    let data:any = loadLocalProperty(this.key, defaultData);
+    if(typeof data == undefined) {
+      return defaultData;
+    }
+
+    return data;
   }
 
   render() {
     const { widget }: any = this.state;
 
     return (
-      <div className="notepad-container">
-        <div className="ui form">
-          <div className="field">
-            <textarea
-              onChange={this.handleChange}
-              value={widget.value}
-            />
-          </div>
-        </div>
+      <div className="notepad-container ui input fluid">
+        <textarea
+          onChange={this.handleChange}
+          value={widget.content}
+        />
       </div>
     );
   }
