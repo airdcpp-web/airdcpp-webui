0a2219d71069d01cc982b8b15366d81daf82ebcd
maksis <maksis@adrenaline-network.com>
2020-08-19 09:21:43 +0300

    HTTP server: support a custom Authorization header to avoid issues with reverse proxies with their own authentication
    https://github.com/airdcpp-web/airdcpp-webclient/issues/330

--- src/utils/HttpUtils.ts
+++ src/utils/HttpUtils.ts
@@ -38,6 +38,9 @@ export const fetchCorsSafeData = async (url: string, isJSON: boolean, options?: 
         headers: {
           'Authorization': LoginStore.authToken,
 
+          // https://github.com/airdcpp-web/airdcpp-webclient/issues/330
+          'X-Authorization': LoginStore.authToken,
+
           // Temp workaround for people upgrading from versions < 2.7.0
           // If the new UI was loaded before restarting the app, a cache expiration time of 
           // one year would have been set for "Not found" errors...
