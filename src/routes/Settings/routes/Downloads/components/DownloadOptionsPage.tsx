dc2ddb228b98c3446dff14083d6478957203bb65
maksis <maksis@adrenaline-network.com>
2021-01-01 11:38:41 +0200

    Add buffer and compression options

--- src/routes/Settings/routes/Downloads/components/DownloadOptionsPage.tsx
+++ src/routes/Settings/routes/Downloads/components/DownloadOptionsPage.tsx
@@ -3,20 +3,53 @@ import RemoteSettingForm from 'routes/Settings/components/RemoteSettingForm';
 import { SettingSectionChildProps } from 'routes/Settings/components/SettingSection';
 
 const Entry = [
-  'segmented_downloads',
-  'min_segment_size',
   'allow_slow_overlap',
   'finished_remove_exit',
   'use_partial_sharing',
 ];
 
-const DownloadOptionsPage: React.FC<SettingSectionChildProps> = props => (
-  <div>
-    <RemoteSettingForm
-      { ...props }
-      keys={ Entry }
-    />
-  </div>
-);
+
+const SegmentsEntry = [
+  'segmented_downloads',
+  'min_segment_size',
+];
+
+const BufferEntry = [
+  'socket_read_buffer',
+  'socket_write_buffer',
+  'buffer_size',
+];
+
+const CompressionEntry = [
+  'compress_transfers',
+  'max_compression',
+];
+
+const DownloadOptionsPage: React.FC<SettingSectionChildProps> = props => {
+  const { translate } = props.moduleT;
+  return (
+    <div>
+      <RemoteSettingForm
+        { ...props }
+        keys={ Entry }
+      />
+      <RemoteSettingForm
+        { ...props }
+        keys={ SegmentsEntry }
+        title={ translate('Segments') }
+      />
+      <RemoteSettingForm
+        { ...props }
+        keys={ CompressionEntry }
+        title={ translate('Compression') }
+      />
+      <RemoteSettingForm
+        { ...props }
+        keys={ BufferEntry }
+        title={ translate('Buffers') }
+      />
+    </div>
+  );
+};
 
 export default DownloadOptionsPage;
