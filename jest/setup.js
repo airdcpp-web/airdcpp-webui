7e23b4c82e00966bce0ff7cf1b945cd62f571011
maksis <maksis@adrenaline-network.com>
2023-03-06 19:48:09 +0200

    Fix tests

--- jest/setup.js
+++ jest/setup.js
@@ -1,25 +1,4 @@
-const storageMock = (function() {
-  var store = {};
-  return {
-    getItem: function(key) {
-      return store[key];
-    },
-    setItem: function(key, value) {
-      store[key] = value.toString();
-    },
-    removeItem: function(key) {
-      delete store[key];
-    },
-    clear: function() {
-      store = {};
-    }
-  };
-});
-
 global.getBasePath = () => '/'
-
-global.localStorage = storageMock();
-global.sessionStorage = storageMock();
 
 window.$ = require('jquery');
 window.jQuery = require('jquery');
