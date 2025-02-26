c8a674bdf5b6d86e31c18bac3c9e3f49acc16cfa
maksis <maksis@adrenaline-network.com>
2025-02-26 20:50:46 +0200

    Try to fix CI

--- vitest.config.common.ts
+++ vitest.config.common.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default {
+  plugins: [react()],
   test: {
     alias: {
       '*': resolve(__dirname, './src'),
