f9cd15a85396ff123a37a3b0f65b8b692c9a12d6
maksis <maksis@adrenaline-network.com>
2025-07-21 19:44:19 +0300

    Add basic tests for the private chat view

---
+++ src/routes/Sidebar/routes/Messages/__tests__/Messages.browser.test.tsx
@@ -0,0 +1,182 @@
+import { afterEach, beforeAll, beforeEach, describe, expect, test, vi } from 'vitest';
+import { RenderResult, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+
+import { getMockServer } from 'airdcpp-apisocket/tests';
+
+import { renderDataRoutes } from '@/tests/render/test-renderers';
+
+import Messages from '../components/Messages';
+import {
+  PrivateChat1,
+  PrivateChat1MessageMe,
+  PrivateChat1MessageOther,
+  PrivateChat1MessagesResponse,
+  PrivateChat2,
+  PrivateChat2MessageOther,
+  PrivateChat2MessagesResponse,
+} from '@/tests/mocks/api/private-chat';
+import { useStoreDataFetch } from '@/components/main/effects/StoreDataFetchEffect';
+import ShareConstants from '@/constants/ShareConstants';
+import { ShareTempItemListResponse } from '@/tests/mocks/api/share';
+import PrivateChatConstants from '@/constants/PrivateChatConstants';
+import { initCommonDataMocks } from '@/tests/mocks/mock-data-common';
+import { setupUserEvent } from '@/tests/helpers/test-form-helpers';
+
+// tslint:disable:no-empty
+describe('Private messages', () => {
+  let server: ReturnType<typeof getMockServer>;
+  const getSocket = async () => {
+    const commonData = await initCommonDataMocks(server);
+
+    // Temp shares
+    server.addRequestHandler(
+      'GET',
+      ShareConstants.TEMP_SHARES_URL,
+      ShareTempItemListResponse,
+    );
+
+    const tempItemAdded = server.addSubscriptionHandler(
+      ShareConstants.MODULE_URL,
+      ShareConstants.TEMP_ITEM_ADDED,
+    );
+
+    const tempItemRemoved = server.addSubscriptionHandler(
+      ShareConstants.MODULE_URL,
+      ShareConstants.TEMP_ITEM_REMOVED,
+    );
+
+    // Messages
+    const onSession1Read = vi.fn();
+    server.addRequestHandler(
+      'POST',
+      `${PrivateChatConstants.SESSIONS_URL}/${PrivateChat1.id}/messages/read`,
+      undefined,
+      onSession1Read,
+    );
+
+    const onSession2Read = vi.fn();
+    server.addRequestHandler(
+      'POST',
+      `${PrivateChatConstants.SESSIONS_URL}/${PrivateChat2.id}/messages/read`,
+      undefined,
+      onSession2Read,
+    );
+
+    server.addRequestHandler(
+      'GET',
+      `${PrivateChatConstants.SESSIONS_URL}/${PrivateChat1.id}/messages/0`,
+      PrivateChat1MessagesResponse,
+    );
+
+    server.addRequestHandler(
+      'GET',
+      `${PrivateChatConstants.SESSIONS_URL}/${PrivateChat2.id}/messages/0`,
+      PrivateChat2MessagesResponse,
+    );
+
+    return { commonData, tempItemAdded, tempItemRemoved, onSession1Read, onSession2Read };
+  };
+
+  const renderLayout = async () => {
+    const { commonData, ...other } = await getSocket();
+
+    const MessageLayoutTest = () => {
+      useStoreDataFetch(true);
+      return (
+        <>
+          <Messages />
+        </>
+      );
+    };
+
+    const routes = [
+      {
+        path: '/messages/:session?/:id?/*',
+        Component: MessageLayoutTest,
+      },
+    ];
+
+    const renderData = renderDataRoutes(routes, commonData, {
+      routerProps: { initialEntries: ['/messages'] },
+    });
+
+    return { ...commonData, ...renderData, ...other };
+  };
+
+  beforeAll(() => {
+    localStorage.setItem('debug', 'true');
+  });
+
+  beforeEach(() => {
+    server = getMockServer();
+  });
+
+  afterEach(() => {
+    server.stop();
+    localStorage.clear();
+  });
+
+  const waitLoaded = async (queryByText: RenderResult['queryByText']) => {
+    // await waitFor(() => queryByText(/Loading sessions/i));
+    await waitForElementToBeRemoved(() => queryByText(/Loading sessions/i));
+
+    // await waitFor(() => queryByText(/Loading messages/i));
+    await waitForElementToBeRemoved(() => queryByText(/Loading messages/i));
+  };
+
+  test('should load messages', async () => {
+    const { getByText, queryByText, socket } = await renderLayout();
+
+    await waitLoaded(queryByText);
+
+    // Check content
+    await waitFor(() => expect(getByText(PrivateChat1MessageMe.text)).toBeTruthy());
+
+    socket.disconnect();
+  });
+
+  test('should handle read messages', async () => {
+    const {
+      getByText,
+      queryByText,
+      socket,
+      mockStoreListeners,
+      onSession1Read,
+      onSession2Read,
+    } = await renderLayout();
+
+    await waitLoaded(queryByText);
+
+    // Send message for the active session
+    mockStoreListeners.privateChat.chatMessage.fire(
+      {
+        ...PrivateChat1MessageOther,
+        text: 'Chat 1 new message',
+        id: PrivateChat1MessageOther.id + 1,
+      },
+      PrivateChat1.id,
+    );
+    await waitFor(() => expect(onSession1Read).toHaveBeenCalledTimes(1));
+
+    // Send message for the background session
+    mockStoreListeners.privateChat.chatMessage.fire(
+      {
+        ...PrivateChat2MessageOther,
+        text: 'Chat 2 new message',
+        id: PrivateChat2MessageOther.id + 1,
+      },
+      PrivateChat2.id,
+    );
+    await waitFor(() => expect(onSession2Read).toHaveBeenCalledTimes(0));
+
+    // Activate the background session
+    const userEvent = setupUserEvent();
+    const session2MenuItem = queryByText(PrivateChat2.user.nicks);
+    await userEvent.click(session2MenuItem!);
+    await waitFor(() => expect(onSession2Read).toHaveBeenCalledTimes(1));
+
+    await waitFor(() => expect(getByText(PrivateChat2MessageOther.text)).toBeTruthy());
+
+    socket.disconnect();
+  });
+});
