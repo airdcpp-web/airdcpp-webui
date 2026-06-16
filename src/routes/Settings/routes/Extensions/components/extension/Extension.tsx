346494d9068064d85ee42f36ea6246f3eed1bd54
Kcchouette <kcchouette@posteo.org>
2026-06-16 14:29:54 +0200

    Replace moment with dayjs
    Replaced moment (~72KB gz) with dayjs (~1.2KB gz) + plugins (relativeTime, calendar, duration, localeData). Updated 5 files: Formatter.ts, LocalizationService.ts, FileViewerElement.tsx, Extension.tsx, ExtensionInfoEntry.tsx. Added 17 unit tests to verify behavior preservation.

--- src/routes/Settings/routes/Extensions/components/extension/Extension.tsx
+++ src/routes/Settings/routes/Extensions/components/extension/Extension.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Moment from 'moment';
+import dayjs from 'dayjs';
 
 import ExtensionConstants from '@/constants/ExtensionConstants';
 
@@ -42,7 +42,7 @@ const Version: React.FC<VersionProps> = ({ title, packageInfo, className, module
         defaultValue: '{{version}} (published {{date}})',
         replace: {
           version: packageInfo.version,
-          date: Moment(packageInfo.date).from(Moment()),
+          date: dayjs(packageInfo.date).fromNow(),
         },
       });
 
